c37f0e9f4ce53d13391e14c00e7e6d260664c9e1
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-04 07:39:41 -0600

    Accumulates values of numbers array using reduce method

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const numbers = [1, -1, 2, 3];
 
-const items = numbers
-  .filter((n) => n >= 0)
-  .map((n) => ({ value: n }))
-  .filter((obj) => obj.value > 1)
-  .map((obj) => obj.value);
-console.log(items);
+// let sum = 0;
+// for (let n of numbers) sum += n;
+
+let sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+
+console.log(sum);
+
+// const items = numbers
+//   .filter((n) => n >= 0)
+//   .map((n) => ({ value: n }))
+//   .filter((obj) => obj.value > 1)
+//   .map((obj) => obj.value);
+// console.log(items);
 
 // const items = filtered.map((n) => "<li>" + n + "</li>");
 
