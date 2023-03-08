4e432bd886d09c9cefc1b4824e409a79da9a96a9
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-08 10:50:04 -0600

    Defines property of defaultLocation with getter and setter

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-// Facotry Funtion
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log("draw");
-    },
-  };
-}
-
-const circle1 = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
-
-const circle = createCircle(1);
-
 function Circle(radius) {
   this.radius = radius;
+  let defaultLocation = { x: 0, y: 0 };
+  this.getDefaultLocation = function () {
+    return defaultLocation;
+  };
   this.draw = function () {
     console.log("draw");
   };
+  Object.defineProperty(this, "defaultLocation", {
+    get: function () {
+      return defaultLocation;
+    },
+    set: function (value) {
+      if (!value.x || !value.y) throw new Error("Invalid location.");
+      defaultLocation = value;
+    },
+  });
 }
 
-const another = new Circle(10);
+const circle = new Circle(10);
+console.log(circle);
 
-for (let key in another) {
-  if (typeof circle[key] !== "function") console.log(key, circle[key]);
-}
+// // Facotry Function
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     draw: function () {
+//       console.log("draw");
+//     },
+//   };
+// }
+
+// const circle1 = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   draw: function () {
+//     console.log("draw");
+//   },
+// };
+
+// const circle = createCircle(1);
+
+// const another = new Circle(10);
+
+// const keys = Object.keys(another);
+// console.log(keys);
+
+// for (let key in another) {
+//   if (typeof circle[key] !== "function") console.log(key, circle[key]);
+// }
