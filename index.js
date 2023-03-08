a474a363eec8bc594efbf7b823b87c39271fb644
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-08 12:28:36 -0600

    Defines properties of a person object

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-function Circle(radius) {
-  this.radius = radius;
-  let defaultLocation = { x: 0, y: 0 };
-  this.getDefaultLocation = function () {
-    return defaultLocation;
-  };
-  this.draw = function () {
-    console.log('draw');
-  };
-  Object.defineProperty(this, 'defaultLocation', {
-    get: function () {
-      return defaultLocation;
-    },
-    set: function (value) {
-      if (!value.x || !value.y) throw new Error('Invalid location.');
-      defaultLocation = value;
-    },
-  });
-}
+let person = { name: 'Justin' };
+
+Object.defineProperty(person, 'name', {
+  writable: false,
+  enumerable: true,
+  configurable: false,
+});
+
+delete person.name;
+
+console.log(person);
+
+// function Circle(radius) {
+//   this.radius = radius;
+//   let defaultLocation = { x: 0, y: 0 };
+//   this.getDefaultLocation = function () {
+//     return defaultLocation;
+//   };
+//   this.draw = function () {
+//     console.log('draw');
+//   };
+//   Object.defineProperty(this, 'defaultLocation', {
+//     get: function () {
+//       return defaultLocation;
+//     },
+//     set: function (value) {
+//       if (!value.x || !value.y) throw new Error('Invalid location.');
+//       defaultLocation = value;
+//     },
+//   });
+// }
 
 // const circle = new Circle(10);
 // console.log(circle);
