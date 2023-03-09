d732af1b50ad1ec261300cb3700dd41f501f20ae
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-08 21:53:41 -0600

    Calls the super constructor in the circle constructor

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function Shape() {}
+function Shape(color) {
+  this.color = color;
+}
 
 Shape.prototype.duplicate = function () {
   console.log('duplicate');
 };
 
 function Circle(radius) {
+  Shape.call(this, color);
+
   this.radius = radius;
 }
 
 Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
 
 Circle.prototype.draw = function () {
   console.log('draw');
