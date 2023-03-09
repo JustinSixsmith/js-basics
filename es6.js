414dc762087325e50d6f312f2593059309bb4694
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-09 10:52:15 -0600

    overrides method in shape class

--- es6.js
+++ es6.js
@@ -1,27 +1,40 @@
 'use strict';
 
 class Shape {
-  constructor(color) {
-    this.color = color;
-  }
-
   move() {
     console.log('move');
   }
 }
 
 class Circle extends Shape {
-  constructor(color, radius) {
-    super(color);
-    this.radius = radius;
-  }
-
-  draw() {
-    console.log('draw');
+  move() {
+    super.move();
+    console.log('circle move');
   }
 }
 
-const c = new Circle('red', 1);
+// class Shape {
+//   constructor(color) {
+//     this.color = color;
+//   }
+
+//   move() {
+//     console.log('move');
+//   }
+// }
+
+// class Circle extends Shape {
+//   constructor(color, radius) {
+//     super(color);
+//     this.radius = radius;
+//   }
+
+//   draw() {
+//     console.log('draw');
+//   }
+// }
+
+// const c = new Circle('red', 1);
 
 // function Circle(radius) {
 //   this.radius = radius;
