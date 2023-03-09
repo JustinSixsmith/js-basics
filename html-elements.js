f7df389a7f14aadad05a97c2cbff2876dc98738e
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-09 01:01:01 -0600

    Creates HTMLImageElement construcotr renders both it and HTMLSelectElement

--- html-elements.js
+++ html-elements.js
@@ -17,8 +17,36 @@ function HTMLSelectElement(items = []) {
   this.removeItem = function (item) {
     this.items.splice(this.items.indexOf(item), 1);
   };
+
+  this.render = function () {
+    return `
+<select>${this.items
+      .map(
+        (item) => `
+  <option>${item}</option>`
+      )
+      .join('')}
+</select>`;
+  };
+}
+
+function HTMLImageElement(src) {
+  this.src = src;
+
+  this.render = function () {
+    return `<img src="${this.src}" />`;
+  };
 }
 
 // HTMLSelectElement.prototype = Object.create(HTMLElement.prototype);
 HTMLSelectElement.prototype = new HTMLElement();
 HTMLSelectElement.prototype.constructor = HTMLSelectElement;
+HTMLImageElement.prototype = new HTMLElement();
+HTMLImageElement.prototype.constructor = HTMLImageElement;
+
+const htmls = [
+  new HTMLSelectElement([1, 2, 3]),
+  new HTMLImageElement('https://something.com'),
+];
+
+for (let element of htmls) console.log(element.render());
